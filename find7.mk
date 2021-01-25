42a195416d2d160be9e6b9e19ede2f55d7bc8160
baolong24 <1667375308@qq.com>
2021-01-25 16:22:38 +0000

    Revert "find7: Inherit common_mini_phone"
    This reverts commit fd2f0d1d27d72ac96ea757595e292d9a7b7a75e4.

--- find7.mk
+++ find7.mk
@@ -65,10 +65,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Recorder
 
-# Recorder
-PRODUCT_PACKAGES += \
-    Recorder
-
 # Shipping API
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
 
